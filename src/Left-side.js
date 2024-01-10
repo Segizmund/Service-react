46ade04753262f67bc23fa1b22a12a111b270fb7
Максим Сын Отца Своего <59877317+Segizmund@users.noreply.github.com>
2024-01-10 14:05:44 +0300

    second

--- src/Left-side.js
+++ src/Left-side.js
@@ -1,18 +1,30 @@
 import Card__technology from './Card-technology.js';
 
 function Left__side() {
+
+  let CardTitle = [{
+    id: 1,
+    Title: 'MelGU-0001',
+    Status: 'Формирование',
+    DateSupply: '12.12.2023',
+    StatusSupply: 'Не подана'
+  }];
+
+  let CardElements = CardTitle
+    .map(Card => <Card__technology
+      id={Card.id} 
+      Title={Card.Title} 
+      Status={Card.Status}
+      DateSupply={Card.DateSupply}
+      StatusSupply ={Card.StatusSupply}/>);
+
   return (
     <div className="Left__side">
     <div>
       <span className="Title__category">Ваши технологии</span>
     </div>
-      <Card__technology 
-      Title="MelGU-0001" 
-      Status="Формирование" 
-      DateSupply="12.12.2023"
-      StatusSupply ="Не подана"/>
-    </div>
-  );
+      {CardElements}
+    </div>);
 }
 
 export default Left__side;
