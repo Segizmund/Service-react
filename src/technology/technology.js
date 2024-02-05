34017caadccfb37c87c31765adcce80412f816a5
Максим Сын Отца Своего <59877317+Segizmund@users.noreply.github.com>
2024-02-05 15:43:07 +0300

    Second

--- src/technology/technology.js
+++ src/technology/technology.js
@@ -5,7 +5,23 @@ function Technology() {
         <div className="Tech__wrapp">
             <Info__technology/>
             <div className="Request__tech">
-                <h6>Запрос</h6>
+                <h3>Запрос</h3>
+               <div className="Checkbox__wrapp">
+               <div>
+                    <input type="checkbox" id="Req1" name="Req1"/>
+                    <label for="Req1">Запрос подается в ответ на предложение</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="Req2" name="Req2"/>
+                    <label for="Req2">Запрос подается от ЦТТ - владельца категории</label>
+                </div>
+               </div> 
+            </div>
+            <div className="Info__org">
+                <div className="Input__text">
+                    <label for="Titleorg">Наименование организации</label>
+                    <input required type="text" id="Titleorg" name="Ttitleorg"/>
+                </div>
             </div>
         </div>
     );
